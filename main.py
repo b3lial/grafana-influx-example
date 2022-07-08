7a32da6d77f1912c592850a4ce5405609114ab14
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-07-09 01:07:46 +0200

    implemented linter warnings

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests
 import os
 import json
 import logging
+import sys
+import requests
 
 grafana_host = "localhost"
 grafana_port = 3000
@@ -12,15 +13,16 @@ ifdb_database = ""
 datasource_name = ""
 ifdb_password = ""
 ifdb_host = ""
-ifdb_port = ""
+ifdb_port = 8086
 ifdb_user = ""
+
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
     # login and open session
     logging.info("trying to login into grafana")
-    grafana_url = os.path.join('http://', '%s:%u' % (grafana_host, grafana_port))
+    grafana_url = os.path.join('http://', f'{grafana_host}:{grafana_port}')
     session = requests.Session()
     login_post = session.post(
         os.path.join(grafana_url, 'login'),
@@ -31,13 +33,13 @@ def main():
         headers={'content-type': 'application/json'})
 
     if login_post.status_code != 200:
-        logging.error("login response {}, aborting".format(login_post.status_code))
-        exit()
+        logging.error("login response %d, aborting", login_post.status_code)
+        sys.exit()
 
     # Get list of datasources
     datasources_get = session.get(os.path.join(grafana_url, 'api', 'datasources'))
     datasources = datasources_get.json()
-    logging.info("grafana server data sources: {}".format(datasources))
+    logging.info("grafana server data sources: %s", datasources)
 
     # Add new datasource
     datasources_put = session.put(
@@ -48,9 +50,12 @@ def main():
             'name': datasource_name,
             'password': ifdb_password,
             'type': 'influxdb_08',
-            'url': 'http://%s:%u' % (ifdb_host, ifdb_port),
+            'url': f'http://{ifdb_host}:{ifdb_port}',
             'user': ifdb_user}),
         headers={'content-type': 'application/json'})
+    if datasources_put.status_code != 200:
+        logging.error("login response %d, aborting", datasources_put.status_code)
+        sys.exit()
 
 if __name__=="__main__":
     main()
